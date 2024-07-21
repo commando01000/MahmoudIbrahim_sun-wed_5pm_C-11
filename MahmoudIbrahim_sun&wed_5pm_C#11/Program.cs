75b1ff908219a21b65022e3d51b6863d5169332d
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-07-22 02:27:56 +0300

    Initial commit

---
+++ MahmoudIbrahim_sun&wed_5pm_C#11/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+
+namespace MahmoudIbrahim_sun_wed_5pm_C_11
+{
+    internal class Program
+    {
+        public static void ReverseArrayList(ArrayList list)
+        {
+            int start = 0;
+            int end = list.Count - 1;
+
+            while (start < end)
+            {
+                object temp = list[start];
+                list[start] = list[end];
+                list[end] = temp;
+
+                start++;
+                end--;
+            }
+        }
+        public static List<int> FilterEvenNumbers(List<int> list)
+        {
+            List<int> evenList = new List<int>();
+            foreach (var item in list)
+            {
+                if (item % 2 == 0)
+                {
+                    evenList.Add(item);
+                }
+            }
+            return evenList;
+        }
+        static void Main(string[] args)
+        {
+            Rangee<int> r = new Rangee<int>(10, 30);
+            int res = r.isInRange(13);
+            if(res == 0)
+            {
+                Console.WriteLine("Value is within the Range !");
+            }
+            else
+            {
+                Console.WriteLine("Value is not within the Range !");
+            }
+
+            Console.WriteLine(r.Length());
+
+            ArrayList list = new ArrayList { 1, 2, 3, 4, 5 };
+
+            ReverseArrayList(list);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            List<int> originalList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            List<int> evenList = FilterEvenNumbers(originalList);
+
+            foreach (var num in evenList)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
---
+++ MahmoudIbrahim_sun&wed_5pm_C#11/Range.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MahmoudIbrahim_sun_wed_5pm_C_11
+{
+    public class Rangee <T> where T : IComparable
+    {
+
+        public T Min { get; set; }
+        public T Max { get; set; }
+        public Rangee (T start, T end)
+        {
+            Min = start;
+            Max = end;
+        }
+
+        public override string? ToString()
+        {
+            return $"{this.Min} - {this.Max}";
+        }
+
+        public int Length ()
+        {
+            return Convert.ToInt32(this.Max) - Convert.ToInt32(this.Min);
+        }
+
+        public int isInRange(T value)
+        {
+            int result = this.CompareTo(this);
+            return result;
+        }
+
+        // implement iComparable
+        public int CompareTo(object? obj)
+        {
+            if (obj == null) return 1;
+            Rangee<T> range = obj as Rangee<T>;
+            if (range != null)
+            {
+                if (this.Min.CompareTo(range.Min) > 0)
+                {
+                    return 1;
+                }
+                else if (this.Min.CompareTo(range.Min) < 0)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Object is not within the Range !");
+            }
+        }
+    }
+}
